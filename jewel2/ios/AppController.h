344f45996995aa8af780108a18605a14f7c767d3
CpyShine <backjy@foxmail.com>
2013-05-22 10:30:42 +0800

    Initial Commit

---
+++ jewel2/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  jewel2AppController.h
+//  jewel2
+//
+//  Created by CpyShine on 13-5-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
